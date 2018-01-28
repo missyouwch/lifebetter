aeb60e68c9a4673b70706990764b0e29c0ec43a7
wangch <missyouwch@gmail.com>
2018-01-28 14:32:30 +0800

    predis

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangch
+ * Date: 2018/1/25
+ * Time: 19:23
+ */
+namespace App\Http\Controllers;
+class UserController extends Controller
+{
+    public function index()
+    {
+        return  'api success';
+
+    }
+}
